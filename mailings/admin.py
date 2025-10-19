01cc12c5ee6c4171a39d1f848eec0f185baa3e63
LimDmitriy <160336426+LimDmitriy@users.noreply.github.com>
2025-10-20 03:16:15 +0900

    Созданы нужные модели, настроена админка

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Mailing, Message, Subscriber, DeliveryStatus
 
-# Register your models here.
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+	list_display = ("fullname", "email", "comment")
+	search_fields = ("fullname", "email")
+	ordering = ("fullname",)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("subject",)
+    search_fields = ("subject",)
+    ordering = ("subject",)
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("message", "status", "start_time", "end_time")
+    list_filter = ("status",)
+    search_fields = ("message__subject",)
+    ordering = ("-start_time",)
+    filter_horizontal = ("subscribers",)
+
+
+@admin.register(DeliveryStatus)
+class DeliveryStatusAdmin(admin.ModelAdmin):
+    list_display = ("mailing", "status", "sent_at")
+    list_filter = ("status", "sent_at")
+    search_fields = ("mailing__message__subject",)
+    ordering = ("-sent_at",)
---
+++ mailings/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .apps import MailingsConfig
+
+app_name = MailingsConfig.name
+
+urlpatterns = []
